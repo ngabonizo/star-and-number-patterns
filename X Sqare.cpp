461b4ef098fe748d033985074a372eb985ab7ccd
NGABO <ngaboizo2000@gmail.com>
2024-10-02 17:48:56 +0200

    24th commit - X sqare pattern

---
+++ X Sqare.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int i,j,N;
+	
+	printf("Enter the number of row/cols: ");
+	scanf("%d",&N);
+	
+	for(i=1; i<=N; i++){
+		for(j=1; j<=N; j++){
+			if(i==j || j==N-i+1 || i==1 || i==N || j==1 || j==N){
+				printf("*");
+			}else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
