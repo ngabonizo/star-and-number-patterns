cb4196f0d27b80d18ce14f7191d61c8fc2bf2657
NGABO <ngaboizo2000@gmail.com>
2024-10-10 01:36:03 +0200

    28th commit -chess pattern

---
+++ chessboardPattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int i,j,N;
+	printf("Enter number of rows/cols: ");
+	printf("%d",&N);
+	
+	for(i=1; i<=N; i++){
+		for(j=1; j<=N; j++){
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
