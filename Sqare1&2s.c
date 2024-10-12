f1203550021d69a02d903249431b22b0202d4fb7
NGABO <ngaboizo2000@gmail.com>
2024-10-12 22:55:18 +0200

    31st commit - sqares1&2

---
+++ Sqare1&2s.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int i,j,N;
+	
+	printf("Enter the value of rows/cols: ");
+	scanf("%d",&N);
+	
+	for(i=1; i<=N; i++){
+		for(j=1; j<=N; j++){
+			if(i==1 || i==N || j==1 || j==N){
+				printf("1");
+			}else{
+				printf("0");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
