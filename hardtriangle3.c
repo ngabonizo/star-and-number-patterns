1d589f7df1438460309854202edc35d3fed0d7c3
NGABO <ngaboizo2000@gmail.com>
2024-11-20 21:16:01 +0200

    2rd hard triangle

---
+++ hardtriangle3.c
@@ -0,0 +1,32 @@
+/**
+ * C program to print the given number pattern
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+
+    printf("Enter rows: ");
+    scanf("%d", &N);
+
+    for(i=1; i<=N; i++)
+    {
+        // Prints the first part of pattern
+        for(j=1; j<=(i*2)-1; j+=2)
+        {
+            printf("%d", j);
+        }
+
+        // Prints the second part of pattern
+        for(j=(i-1)*2-1; j>=1; j-=2)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
