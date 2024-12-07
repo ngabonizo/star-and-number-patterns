7d83a024e441766108febc2884c42c03746a0983
NGABO <ngaboizo2000@gmail.com>
2024-12-07 11:40:36 +0200

    12th hard triangle

---
+++ 12th hard triangle.c
@@ -0,0 +1,31 @@
+/**
+ * C program to print the given number pattern
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, diff, value, N;
+
+    printf("Enter rows: ");
+    scanf("%d", &N);
+
+    diff = 1;
+    value = 1;
+
+    for(i=1; i<=N; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("%-3d", value);
+
+            value += diff; // Computes the next value to be printed
+            diff++;        // Increments the difference
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
