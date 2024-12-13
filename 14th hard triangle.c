15c161d8ca05566b37ee79d82f6f4d9b6d2b52b0
NGABO <ngaboizo2000@gmail.com>
2024-12-13 06:20:05 +0200

    14th hard triangle

---
+++ 14th hard triangle.c
@@ -0,0 +1,32 @@
+/**
+ * C program to print the given number pattern
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+
+    printf("Enter rows: ");
+    scanf("%d", &N);
+
+    for(i=1; i<=N; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            if(i <= (N/2))
+            {
+                printf("%d", i);
+            }
+            else
+            {
+                printf("%d", (N - i + 1));
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
